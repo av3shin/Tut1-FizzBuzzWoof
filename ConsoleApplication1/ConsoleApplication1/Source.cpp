de8dbe61298b0bb4212f9174ef86348ce9e3742d
Aveshin Naidoo <av3shin@gmail.com>
2016-02-21 12:43:03 +0200

    Creation of woof branch and implementation of solution 2

--- ConsoleApplication1/ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -10,12 +10,13 @@ Merge into master branch
 using namespace std;
 int main()
 {
-	int a, b, c; //Creaton of condition variables
-	for (int i = 1; i < 21; i++) //The for loop that will run from 1-20
+	int a, b, c, d; //Creaton of condition variables
+	for (int i = 1; i < 51; i++) //The for loop that will run from 1-20
 	{
 		a = i % 3; //The variables are being used to test the conditions
 		b = i % 5;
 		c = a + b;
+		d = i % 7;
 
 		if (c == 0)
 			cout << "fizz buzz" << endl;
@@ -23,6 +24,8 @@ int main()
 			cout << "buzz" << endl;
 		else if (a == 0)
 			cout << "fizz" << endl;
+		else if (d == 0)
+			cout << "woof" << endl;
 		else
 			cout << i << endl;
 	}
